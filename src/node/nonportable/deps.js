76011e49ae9fff6e4548a2c523734840e5d3f76d
Darius J Chuck <darius.j.chuck+github@gmail.com>
2022-12-12 04:44:41 +0100

    add fetchText

--- src/node/nonportable/deps.js
+++ src/node/nonportable/deps.js
@@ -1,2 +1,22 @@
 export {dirname, join, extname, basename, isAbsolute} from 'node:path'
 export {readTextFileSync, readStdinText, writeTextFileSync, mkdirRecursiveSync, existsSync} from './io.js'
+
+import {get} from 'node:https'
+
+export const fetchText = async url => {
+  return new Promise((resolve, reject) => {
+    get(url, res => {
+      let body = ""
+
+      res.on("data", (chunk) => {
+        body += chunk
+      })
+
+      res.on("end", () => {
+        resolve(body)
+      })
+    }).on('error', (err) => {
+      reject(err)
+    })
+  })
+}
