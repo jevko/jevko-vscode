1a573bc45e6aba8552b4c6f26bd5151cfdb0947a
Darius J Chuck <darius.j.chuck+github@gmail.com>
2022-12-16 17:47:47 +0100

    make jevko-interface.js an external dependency

--- src/extension.ts
+++ src/extension.ts
@@ -2,7 +2,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import {main} from './node/bundle.js'
+import {main} from 'jevko-interface.js'
 
 ////////////////////////////////              
 // This method is called when your extension is activated
---
+++ src/modules.d.ts
@@ -0,0 +1,1 @@
+declare module 'jevko-interface.js';
